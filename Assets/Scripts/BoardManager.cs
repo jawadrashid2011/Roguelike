716547f037cf275856c953f12d80ae555372e855
Jawad Rashid <jawadrashid2010@gmail.com>
2017-10-12 04:01:16 +0500

    2. Level Generation - 02 - Writing Game Manager Complete

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -93,7 +93,7 @@ public class BoardManager : MonoBehaviour
 		InitializeList ();
 		LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
 		LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
-		int enemyCount = (int)Mathf.Log (level, 2f);
+		int enemyCount = (int)Mathf.Log(level, 2f);
 		LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
 		Instantiate (exit, new Vector3 (columns - 1, rows - 1, 0f), Quaternion.identity);
 	}
