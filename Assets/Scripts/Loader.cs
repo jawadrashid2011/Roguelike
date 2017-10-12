9edafbbda541334ddd9de328dcab1220ee76bf9f
Jawad Rashid <jawadrashid2010@gmail.com>
2017-10-12 07:39:32 +0500

    Completed Project. Made changes for android.

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -6,10 +6,29 @@ public class Loader : MonoBehaviour {
 
 	public GameObject gameManager;
 
+	#if UNITY_IOS ||  UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+	private float sW;
+	private float sH;
+	#endif
+
 	// Use this for initialization
 	void Awake () {
 		if (GameManager.instance == null)
 			Instantiate (gameManager);
 	}
+
+	#if UNITY_IOS ||  UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+	void Update()
+	{
+		sW = Screen.width;
+		sH = Screen.height;
+
+		if (Screen.width < Screen.height) {
+			Camera.main.orthographicSize = (5f / (sW / sH));
+		} else if (Screen.width > Screen.height) {
+			Camera.main.orthographicSize = 5;
+		}
+	}
+	#endif
 
 }
